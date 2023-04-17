f16b880363aa4f91d002711f01cf89d70cd44925
RakhmaIs <105439663+RakhmaIs@users.noreply.github.com>
2023-04-17 13:19:11 +0300

    Update FilmDto.java

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/FilmDto.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/FilmDto.java
@@ -3,8 +3,9 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.Duration;
+
 import java.time.LocalDate;
+
 @Builder
 @Data
 public class FilmDto {

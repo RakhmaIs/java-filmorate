300a127d773f8e91d37b670aa667be01c4c967ee
RakhmaIs <105439663+RakhmaIs@users.noreply.github.com>
2023-04-17 13:48:43 +0300

    Update FilmService

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -6,8 +6,9 @@ import ru.yandex.practicum.filmorate.model.FilmDto;
 import java.util.List;
 
 public interface FilmService {
-
     FilmDto createFilm(Film film);
+
     FilmDto updateFilm(Film film);
+
     List<FilmDto> readAllFilms();
 }
